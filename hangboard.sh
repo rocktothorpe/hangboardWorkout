4536c635942811ead2c281c6b78de164fdbf3078
Raquel Bonilla <raquelb.2014@gmail.com>
2020-09-10 12:39:28 -0700

    implemented support to play your fave send jams while you grind it out

--- hangboard.sh
+++ hangboard.sh
@@ -19,43 +19,60 @@ updateTime () {
   done
 }
 
-for i in $(seq 11); do
-  case "$i" in
-    1)
-      EXERCISE="15 second hang, 3 pull ups on jugs."
-      ;;
-    2)
-      EXERCISE="20 second hang on crimp. Two pull ups on large edge."
-      ;;
-    3)
-      EXERCISE="20 second hang on large edge. 20 second bent arm hang on pocket."
-      ;;
-    4)
-      EXERCISE="30 second hang on jug, 2 pull-ups."
-      ;;
-    5)
-      EXERCISE="20 second hang, large edge, 4 pull ups large edge."
-      ;;
-    6)
-      EXERCISE="3 offset pull ups on jug and crimp."
-      ;;
-    8)
-      EXERCISE="15 knee raises on sloper, 10 second hang on large edge."
-      ;;
-    9)
-      EXERCISE="20 second hang, large edge."
-			;;
-		10)
-			EXERCISE="15 second hang on pocket, 3 pull ups on jug."
-			;;
-    11)
-      EXERCISE="hang as long as possible, long edge."
-      ;;
-  esac
+keepRunning=$true;
 
-  echo "$EXERCISE";
-	updateTime;
-  say "NEXT";
-done
+while $keepRunning
+do
+
+  if [ $# -gt 0 ]
+    then
+       afplay $1 &>/dev/null &
+  fi
+
+  for i in $(seq 1); do
+    case "$i" in
+      1)
+        EXERCISE="15 second hang, 3 pull ups on jugs."
+        ;;
+      2)
+        EXERCISE="20 second hang on crimp. Two pull ups on large edge."
+        ;;
+      3)
+        EXERCISE="20 second hang on large edge. 20 second bent arm hang on pocket."
+        ;;
+      4)
+        EXERCISE="30 second hang on jug, 2 pull-ups."
+        ;;
+      5)
+        EXERCISE="20 second hang, large edge, 4 pull ups large edge."
+        ;;
+      6)
+        EXERCISE="3 offset pull ups on jug and crimp."
+        ;;
+      8)
+        EXERCISE="15 knee raises on sloper, 10 second hang on large edge."
+        ;;
+      9)
+        EXERCISE="20 second hang, large edge."
+  			;;
+  		10)
+  			EXERCISE="15 second hang on pocket, 3 pull ups on jug."
+  			;;
+      11)
+        EXERCISE="hang as long as possible, long edge."
+        ;;
+    esac
+
+    echo "$EXERCISE";
+  	updateTime;
+    say "NEXT";
+  done;
+
+  keepRunning=$false;
+done;
+
+KILL $!
+
+
 
 
